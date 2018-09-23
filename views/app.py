1984d2c55542662fdfed1b55d2d1f56ff8b71d3d
Guilherme Lima <guilherme.oliveira95@fatec.sp.gov.br>
2018-09-23 17:57:48 -0300

    Nova forma de fazer download da imagem do mapa

--- views/app.py
+++ views/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-from flask import render_template, request, flash
+from flask import Flask, render_template, request, flash
 from flask_googlemaps import GoogleMaps, Map
 from flask_compress import Compress
-import os
+from urllib import request as request_urllib
+from io import BytesIO
+from PIL import Image
 import googlemaps
-from views import GoogleMapDownloader
+import os
 
 # Configura a aplicação, os diretorios de CSS, JS, Imagens e fontes
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -14,10 +15,11 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'WYZ')
 # GZIP - Utilizado para compactar a pagina
 gzip = Compress(app)
 
-# Configura a api do Gmaps
+# Configura as apis do Gmaps
+# API que cria o mapa na tela
 GoogleMaps(app, key="AIzaSyBybFqISUIGfRoLJoSyDUOa_4N4pRUIF8g")
+# API que faz a busca dos endereços
 gmaps = googlemaps.Client(key='AIzaSyBybFqISUIGfRoLJoSyDUOa_4N4pRUIF8g')
-
 
 # Página de inicio
 @app.route('/')
@@ -28,7 +30,7 @@ def index():
 @app.route('/mapa')
 def mapa():
     endereco = ''
-    # Visualização Padrão - Brazil
+    # Visualização Padrão - Brasil
     mapa = maps(-21.9697, -47.8199, 5)
     # Se existir texto no campo endereço da tela
     if (request. args.get('endereco')):
@@ -44,27 +46,36 @@ def mapa():
             # Define a visualização padrão do mapa que será visualizado pelo usuário
             mapa = maps(latitude, longitude, 17)
             # Salva a imagem no diretorio
-            #salva_imagem(latitude,longitude)
+            #salvar_imagem(latitude,longitude)
             # Apresenta mensagem de sucesso
             flash("Local encontrado com sucesso!")
-        except:
+        except Exception as e:
+            # Apresenta mensagem de erro para o usuário
             flash("Erro ao buscar local")
+            # Printa o erro no terminal
+            print(e)
     # Se não existir texto no campo de endereço
     # Verifica se existe texto no campo latitude e longitude
     elif (request. args.get('latitude') and request. args.get('longitude')):
         try:
+            # Recebe a latitude do input
             latitude = request. args.get('latitude')
+            # Recebe a longitude do input
             longitude = request. args.get('longitude')
+            # Define a visualização padrão do mapa que será visualizado pelo usuário
             mapa = maps(latitude, longitude, 17)
             # Salva a imagem no diretorio
-            #salva_imagem(latitude,longitude)
+            #salvar_imagem(latitude, longitude)
             # Apresenta mensagem de sucesso
             flash("Local encontrado com sucesso!")
-        except:
-            flash("Erro ao buscar local")
+        # Se acontecer algum erro
+        except Exception as e:
+            # Apresenta mensagem de erro para o usuário
+            flash("Não foi possível encontrar o local")
+            # Printa no terminal o erro
+            print(e)
 
     return render_template('mapa.html', titulo="Mapa", movingmap=mapa)
-
 
 # Retorna uma instancia do tipo MAP com os dados para criar o mapa
 def maps(latitude, longitude, zoom):
@@ -87,17 +98,41 @@ def maps(latitude, longitude, zoom):
         markers=[(latitude, longitude)]
     )
 
-# Salvar a imagem em alta qualidade
-def salva_imagem(latitude, longitude):
-    # Instancia um objeto do GoogleMapDownloads
-    gmd = GoogleMapDownloader.GoogleMapDownloader(float(latitude), float(longitude), 17)
-    # Gera a imagem
-    img = gmd.generateImage()
-    # Cria um nome para imagem
-    nomeImagem = str(latitude) + str(longitude) + ".png"
-    # Salva a imagem
-    img.save(nomeImagem)
-
+# Gera uma imagem do local pesquisado no Google Maps
+def salvar_imagem(latitude, longitude):
+    try:
+        # Define o tipo de visualização do mapa
+        # Tipos
+            # roadmap - Exibe a visualização padrão do mapa
+            # satellite - Exibe imagens de satelites do Google Earth
+            # hybrid - Exibe uma mistura satellite com roadmap
+            # terrain - exibe um mapa físico com base nas informações do terreno.
+        maptype = "satellite"
+        # Define o tamanho da imagem que será baixada
+        imageSize = '640x640'
+        # Zoom padrão do mapa
+        zoom = "17"
+        # Cria a Visualizaçao do mapa com a API do google
+        # Adiciona a Latitude, longitude, tamanho da imagem, zoom e tipo de mapa na url
+        url = 'http://maps.googleapis.com/maps/api/staticmap?center=' + str(latitude) + ',' + str(
+            longitude) + '&size=' + imageSize + '&zoom=' + zoom + '&maptype=' + maptype
+        # Abre a url e faz a leitura da pagina
+        with request_urllib.urlopen(url) as url:
+            page = url.read()
+        # Aloca o resultado da página na memoria
+        buffer = BytesIO(page)
+        # Abre e identifica a imagem recebida
+        image = Image.open(buffer)
+        # Mostra a imagem recebida para o usuário
+        image.show()
+        # Cria um nome para imagem - latitudelongitude.png
+        nomeImagem = str(latitude) + str(longitude) + ".png"
+        # Salva a imagem no diretorio do projeto
+        image.save(nomeImagem)
+    # Se acontecer algum erro
+    except Exception as e:
+        # Printa o erro no terminal
+        print(e)
 
 if __name__ == '__main__':
     app.run()
