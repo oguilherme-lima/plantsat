ff9850045d08db2f9c6353b775649a875a89c630
Guilherme Lima <guilherme.oliveira95@fatec.sp.gov.br>
2018-10-06 21:55:18 -0300

    Mudança no layout da página

--- views/app.py
+++ views/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_googlemaps import GoogleMaps, Map
 from flask_compress import Compress
 from urllib import request as request_urllib
@@ -6,6 +6,9 @@ from io import BytesIO
 from PIL import Image
 import googlemaps
 import os
+
+# TODO IMPORTAR API
+# from NOME_DO_ARQUIVO_PY_DA_IA import NOME_METODO_IA
 
 # Configura a aplicação, os diretorios de CSS, JS, Imagens e fontes
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -16,24 +19,53 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'WYZ')
 gzip = Compress(app)
 
 # Configura as apis do Gmaps
+# Chave de utilização Google Maps
+gmaps_key = "AIzaSyC18sKb1tmkH6aj3XHTjG-3V6XHIlINw-k"
 # API que cria o mapa na tela
-GoogleMaps(app, key="AIzaSyBybFqISUIGfRoLJoSyDUOa_4N4pRUIF8g")
+GoogleMaps(app, key=gmaps_key)
 # API que faz a busca dos endereços
-gmaps = googlemaps.Client(key='AIzaSyBybFqISUIGfRoLJoSyDUOa_4N4pRUIF8g')
+gmaps = googlemaps.Client(key=gmaps_key)
 
 # Página de inicio
 @app.route('/')
 def index():
     return render_template('index.html', titulo="PlantSat")
 
+# Pagina com o resultado
+@app.route('/resultado')
+def resultado():
+    # Recebe o nome da imagem da sessão
+    nomeImagem = session.get('nomeImagem', None)
+    # Recebe o resultado da IA da sessão
+    e_laranja = session.get('e_laranja', None)
+    return render_template('resultado.html', titulo='Resultado', nomeImagem=nomeImagem, e_laranja=e_laranja)
+
 # Pagina do mapa
 @app.route('/mapa')
 def mapa():
     endereco = ''
+    # Boolean que define se existe plantação ou não
+    # Recebe o resultado da IA
+    e_laranja = False
+    processar = True
     # Visualização Padrão - Brasil
     mapa = maps(-21.9697, -47.8199, 5)
+    # Input do frontend que recebe o valor da latitude
+    latitude = ''
+    # Input do frontend que recebe o valor da longitude
+    longitude = ''
+    # Recebe o nome da imagem
+    nomeImagem = ''
+    # Se existir texto no botão processar
+    if (request. args.get('processar')):
+        # Salva a imagem
+        # Retorna o nome da imagem e salva na sessão
+        # Retorna 0 ou 1 de acordo com o resultado da IA e salva a sessão
+        session['nomeImagem'], session['e_laranja'] = salvar_imagem(request. args.get('latitude'), request. args.get('longitude'))
+        # Redireciona para a página de resultado
+        return redirect(url_for('resultado'))
     # Se existir texto no campo endereço da tela
-    if (request. args.get('endereco')):
+    elif (request. args.get('endereco')):
         # Recebe o endereço do input
         endereco = request. args.get('endereco')
         try:
@@ -44,9 +76,7 @@ def mapa():
             # Recebe a longitude do retorno da API
             longitude = geocode_result[0]['geometry']['location']['lng']
             # Define a visualização padrão do mapa que será visualizado pelo usuário
-            mapa = maps(latitude, longitude, 17)
-            # Salva a imagem no diretorio
-            #salvar_imagem(latitude,longitude)
+            mapa = maps(latitude, longitude, 18)
             # Apresenta mensagem de sucesso
             flash("Local encontrado com sucesso!")
         except Exception as e:
@@ -64,8 +94,6 @@ def mapa():
             longitude = request. args.get('longitude')
             # Define a visualização padrão do mapa que será visualizado pelo usuário
             mapa = maps(latitude, longitude, 17)
-            # Salva a imagem no diretorio
-            #salvar_imagem(latitude, longitude)
             # Apresenta mensagem de sucesso
             flash("Local encontrado com sucesso!")
         # Se acontecer algum erro
@@ -74,19 +102,23 @@ def mapa():
             flash("Não foi possível encontrar o local")
             # Printa no terminal o erro
             print(e)
+    else:
+        processar = False
 
-    return render_template('mapa.html', titulo="Mapa", movingmap=mapa)
+    return render_template('mapa.html', titulo="Mapa", movingmap=mapa, endereco_salvo=endereco.title(), latitude=latitude,
+                           longitude=longitude, e_laranja=e_laranja, nomeImagem=nomeImagem, processar=processar)
 
 # Retorna uma instancia do tipo MAP com os dados para criar o mapa
 def maps(latitude, longitude, zoom):
     return Map(
         identifier="movingmap",
         varname="movingmap",
-        # latitude padrão quando abrir o mapa
+        # Visualização padrão do mapa
+        # latitude padrão
         lat=latitude,
-        # longitude padrão quando abrir o mapa
+        # longitude padrão
         lng=longitude,
-        # Distancia do mapa
+        # Zoom do mapa
         zoom=zoom,
         # Tipo de visualização do mapa
         maptype="SATELLITE",
@@ -94,8 +126,15 @@ def maps(latitude, longitude, zoom):
         language="pt",
         # Tamanho do mapa
         style="height:400px;width:100%;",
-        # Pontos do mapa
-        markers=[(latitude, longitude)]
+        # Remove todos os botões do mapa e iterações com o mapa
+        # Permanece apenas clique para movimentar mapa
+        zoom_control=False,
+        maptype_control=False,
+        scale_control=False,
+        streetview_control=False,
+        rotate_control=False,
+        fullscreen_control=False,
+        scroll_wheel=False
     )
 
 # Gera uma imagem do local pesquisado no Google Maps
@@ -109,13 +148,13 @@ def salvar_imagem(latitude, longitude):
             # terrain - exibe um mapa físico com base nas informações do terreno.
         maptype = "satellite"
         # Define o tamanho da imagem que será baixada
-        imageSize = '640x640'
+        imageSize = '800x800'
         # Zoom padrão do mapa
-        zoom = "17"
+        zoom = "18"
         # Cria a Visualizaçao do mapa com a API do google
-        # Adiciona a Latitude, longitude, tamanho da imagem, zoom e tipo de mapa na url
+        # Adiciona a Latitude, longitude, tamanho da imagem, zoom, tipo de mapa e key da API da Google na url
         url = 'http://maps.googleapis.com/maps/api/staticmap?center=' + str(latitude) + ',' + str(
-            longitude) + '&size=' + imageSize + '&zoom=' + zoom + '&maptype=' + maptype
+            longitude) + '&size=' + imageSize + '&zoom=' + zoom + '&maptype=' + maptype + '&key=' + gmaps_key
         # Abre a url e faz a leitura da pagina
         with request_urllib.urlopen(url) as url:
             page = url.read()
@@ -123,12 +162,15 @@ def salvar_imagem(latitude, longitude):
         buffer = BytesIO(page)
         # Abre e identifica a imagem recebida
         image = Image.open(buffer)
-        # Mostra a imagem recebida para o usuário
-        image.show()
-        # Cria um nome para imagem - latitudelongitude.png
-        nomeImagem = str(latitude) + str(longitude) + ".png"
+        # Cria um nome para imagem - latitudelongitude.png que será utilizada para a API
+        nomeImagem = "static/downloaded_images/" + str(latitude) + str(longitude) + ".png"
         # Salva a imagem no diretorio do projeto
         image.save(nomeImagem)
+        # Recebe o retorno da API
+        e_laranja = True # TODO - substituir por metodo da api que retorna se é laranja
+        # Remove o caminho do nome da imagem
+        nomeImagem =  str(latitude) + str(longitude) + ".png"
+        return nomeImagem, e_laranja
     # Se acontecer algum erro
     except Exception as e:
         # Printa o erro no terminal
